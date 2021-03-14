8442ba854da025acf12954f232576c5469461aaa
Avdeev Aleksey <avdeev.hexer@yandex.ru>
2021-03-14 20:31:56 +0300

    Change a test.

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -4,37 +4,69 @@ import org.junit.jupiter.api.Assertions.*
 internal class MainKtTest {
 
     @Test
-    fun main() {
+    fun printMyAmountСommission_MasterMaestroTest() {
         val expected = 2018
         val cardType:Int = 1
         val amountPreviousTransfer:Int = 200
         val amount:Int = 3000
-
-        val result = printMyAmountСommission(cardType, amountPreviousTransfer, amount)
-
+        val result = printMyAmountСommission(cardType,amountPreviousTransfer,amount)
         assertEquals(expected, result)
-
     }
+//
+//    @Test
+//    fun printMyAmountСommission_VisaMirTest() {
+//        val expected = 3500
+//        val cardType:Int = 2
+//        val amountPreviousTransfer:Int = 200
+//        val amount:Int = 46666
+//        val result = printMyAmountСommission(cardType,amountPreviousTransfer,amount)
+//        assertEquals(expected, result)
+//    }
+//
+//    @Test
+//    fun printMyAmountСommission_VKTest() {
+//        val expected = 0
+//        val cardType:Int = 0
+//        val amountPreviousTransfer:Int = 0
+//        val amount:Int = 466666
+//        val result = printMyAmountСommission(cardType,amountPreviousTransfer,amount)
+//        assertEquals(expected, result)
+//    }
+//
+//    @Test
+//    fun giveMyMasterCardMaestro_testOutOfRange() {
+//        val expected = 0
+//        val amountPreviousTransfer:Int = 200
+//        val amount:Int = 2000
+//        val result = giveMyMasterCardMaestro(amountPreviousTransfer,amount)
+//        assertEquals(expected, result)
+//    }
+//
+//    @Test
+//    fun giveMyMasterCardMaestro_testInRange() {
+//        val expected =2018
+//        val amountPreviousTransfer:Int = 500
+//        val amount:Int = 2000
+//        val result = giveMyMasterCardMaestro(amountPreviousTransfer,amount)
+//        assertEquals(expected, result)
+//    }
+//
+//    @Test
+//    fun giveMyVisaMir_testInRange() {
+//        val expected = 3500
+//        val amountPreviousTransfer:Int = 500
+//        val amount:Int = 2000
+//        val result = giveMyVisaMir(amountPreviousTransfer,amount)
+//        assertEquals(expected, result)
+//    }
+//
+//    @Test
+//    fun giveMyVisaMir_testOutOfRange() {
+//        val expected = 5025
+//        val amountPreviousTransfer:Int = 500
+//        val amount:Int = 670000
+//        val result = giveMyVisaMir(amountPreviousTransfer,amount)
+//        assertEquals(expected, result)
+//    }
 
-    @Test
-    fun printMyAmountСommission(cardType: Int, amountPreviousTransfer: Int, amount: Int):Int {
-        return when (cardType) {
-            1 -> giveMyMasterCardMaestro(amountPreviousTransfer, amount)
-            2 -> giveMyVisaMir(amount)
-            else -> 0
-        }
-    }
-
-    @Test
-    fun giveMyMasterCardMaestro(amountPreviousTransfer:Int, amount: Int):Int {
-        return when (amountPreviousTransfer) {
-            in 300 until 75000 -> 0
-            else -> (amount * 0.006 + 2000).toInt()
-        }
-    }
-
-    @Test
-    fun giveMyVisaMir(amount: Int, minCommission: Int = 3500):Int {
-        return if (amount*0.0075<=minCommission) minCommission else (amount*0.0075).toInt()
-    }
 }
